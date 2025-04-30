5378620d3d89249f9d1ab224665d0a4cc694ee57
Whanss <elyaivanmey@gmail.com>
2025-04-30 16:26:52 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+route :: get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/mahasiswa/{merk}', function ($merk) {
+    return "tampilkan data mahasiswa bernama $merk";
+});
+
+Route::get('/stok_barang/{nama_barang}/{merk?}', function ($nama_barang, $merk = 'samsung') {
+    return "cek sisa stok untuk : $nama_barang $merk";
+});
+
+Route::get('/user/{id}', function ($id) {
+    return "Tampilkan User dengan id = $id";
+})->where('id', '([A-Za-z]{2}[0-9]+|[0-9]+)');
+
